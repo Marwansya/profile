c642120690a51961f6d926251748248f03da7053
Marwansya <46847842+Marwansya@users.noreply.github.com>
2025-08-20 18:05:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
       burger.classList.toggle('active');
       menu.classList.toggle('active');
     });
+    
+const audio = document.getElementById('btn-sound');
+
+document.querySelectorAll('.btn-sosmed').forEach(button => {
+  button.addEventListener('click', () => {
+    audio.currentTime = 0;
+    audio.play();
+
+    // redirect setelah suara selesai atau beberapa saat
+    setTimeout(() => {
+      window.open(button.dataset.link, "_blank"); // buka di tab baru
+    }, 300); // 300ms setelah bunyi, bisa sesuaikan
+  });
+});
